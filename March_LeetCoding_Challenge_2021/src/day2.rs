855fd3502183aa6d46439111b38af46adcd02475
Nackan <59600727+RyosukeNAKATA@users.noreply.github.com>
2021-03-03 13:09:50 +0900

    add questions

--- March_LeetCoding_Challenge_2021/src/day2.rs
+++ March_LeetCoding_Challenge_2021/src/day2.rs
@@ -1,3 +1,9 @@
+/*
+Question.
+You have a set of integers s, which originally contains all the numbers from 1 to n. Unfortunately, due to some error, one of the numbers in s got duplicated to another number in the set, which results in repetition of one number and loss of another number.
+You are given an integer array nums representing the data status of this set after the error.
+Find the number that occurs twice and the number that is missing and return them in the form of an array.
+*/
 impl Solution {
     pub fn find_error_nums(nums: Vec<i32>) -> Vec<i32> {
         return output;
