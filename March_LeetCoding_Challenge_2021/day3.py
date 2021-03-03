206d6db3d93692b57ac7810337ad021a666b5bb8
Nackan <59600727+RyosukeNAKATA@users.noreply.github.com>
2021-03-03 17:39:56 +0900

    accepted day3.py

---
+++ March_LeetCoding_Challenge_2021/day3.py
@@ -0,0 +1,12 @@
+"""
+Question.
+Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+Follow up: Could you implement a solution using only O(1) extra space complexity and O(n) runtime complexity?
+"""
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        diff_sum = len(nums) * (len(nums) + 1) / 2 - sum(nums) 
+        output = int(diff_sum)
+        return output
+        
--- March_LeetCoding_Challenge_2021/src/day2.rs
+++ March_LeetCoding_Challenge_2021/src/day2.rs
@@ -4,8 +4,25 @@ You have a set of integers s, which originally contains all the numbers from 1 t
 You are given an integer array nums representing the data status of this set after the error.
 Find the number that occurs twice and the number that is missing and return them in the form of an array.
 */
+use std::vec;
+// use std::iter::Sum;
 impl Solution {
     pub fn find_error_nums(nums: Vec<i32>) -> Vec<i32> {
+        let mut output: Vec<i32> = Vec::new();
+        let mut list: Vec<i32> = Vec::new();
+        let mut sum = 0;
+        for i in nums.iter() {
+            sum += nums;
+        }
+        let diff_sum: i32 = nums.len() * (nums.len() + 1) / 2 - sum;
+        for num in nums.iter() {
+            if num in list {
+                output.push(num)
+                output.push(num+diff_sum)
+                break
+            }
+            list.push(num)
+        }
         return output;
     }
 }
