c098e8d45c6a82721f32aaf249f7fad7a0940b3c
Nackan <59600727+RyosukeNAKATA@users.noreply.github.com>
2021-03-07 01:58:11 +0900

    accepted day6.py

---
+++ March_LeetCoding_Challenge_2021/day6.py
@@ -0,0 +1,16 @@
+"""
+Question.
+A valid encoding of an array of words is any reference string s and array of indices indices such that:
+- words.length == indices.length
+- The reference string s ends with the '#' character.
+- For each index indices[i], the substring of s starting from indices[i] and up to (but not including) the next '#' character is equal to words[i].
+Given an array of words, return the length of the shortest reference string s possible of any valid encoding of words.
+"""
+class Solution:
+    def minimumLengthEncoding(self, words: List[str]) -> int:
+        word_set = set(words)
+        for word in words:
+            if word in word_set:
+                for i in range(1, len(word)):
+                    word_set.discard(word[i:])
+        return len("#".join(list(word_set))) + 1
