3c8b0bc26a651c1ccd7efb76e92fe03f084ac975
Nackan <59600727+RyosukeNAKATA@users.noreply.github.com>
2021-03-13 16:21:28 +0900

    accepted day12.py

---
+++ March_LeetCoding_Challenge_2021/day12.py
@@ -0,0 +1,14 @@
+"""
+Question: Check If a String Contains All Binary Codes of Size K
+Given a binary string s and an integer k.c
+Return True if every binary code of length k is a substring of s. Otherwise, return False.
+"""
+class Solution:
+    def hasAllCodes(self, s: str, k: int) -> bool:
+        set_list = set()
+        for i in range(len(s)-k+1):
+            set_list.add(s[i:i+k])
+        if len(set_list) == 2**k:
+            return True
+        else:
+            return False
