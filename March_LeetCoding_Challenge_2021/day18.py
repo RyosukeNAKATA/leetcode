5803285893c91bc5cdbd772f3fc38cdef1e320ff
Nackan <59600727+RyosukeNAKATA@users.noreply.github.com>
2021-03-18 23:50:24 +0900

    Update day18.py

--- March_LeetCoding_Challenge_2021/day18.py
+++ March_LeetCoding_Challenge_2021/day18.py
@@ -5,3 +5,41 @@ for j in range(4):
         l.pop(j)
 
 print(l)
+
+class Solution:
+    def wiggleMaxLength(self, nums: List[int]) -> int:
+        output = 0
+        def posistive(nums: list[int], n):
+            diff = nums[n+1] - nums[n]
+            if diff > 0:
+                output += 1
+                negative(nums, n+1)
+            else:
+                nums.pop(n+1)
+                posistive(nums, n)
+
+        def negative(nums: list[int], n):
+            diff = nums[n+1] - nums[n]
+            if diff < 0:
+                output += 1
+                posistive(nums, n+1)
+            else:
+                nums.pop(n+1)
+                negative(nums, n)
+        
+        k = 0
+        num = nums[k] - nums[k+1]
+        for _ in range(len(nums)):
+            if num > 0:
+                output += 1
+                posistive(num, k+1)
+                break
+            elif num < 0:
+                output += 1
+                negative(num, k+1)
+                break
+            else:
+                nums.pop(k+1)
+                k += 1
+
+        return output
